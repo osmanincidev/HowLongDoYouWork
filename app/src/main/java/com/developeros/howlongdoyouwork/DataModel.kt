b32671e59df822e870b49b20bf91a068da4bc649
osmanincidev <90893338+osmanincidev@users.noreply.github.com>
2021-12-09 22:09:32 +0300

    Delete app directory

--- app/src/main/java/com/developeros/howlongdoyouwork/DataModel.kt
+++
@@ -1,8 +0,0 @@
-package com.developeros.howlongdoyouwork
-
-class DataModel(var work:String,var Running:Boolean,time:Long) {
-    var hour = time/3600
-    var minutes = (time%3600)/60
-    var seconds = time%60
-    var Time : String = String.format("%02d:%02d:%02d", hour, minutes, seconds)
-}
